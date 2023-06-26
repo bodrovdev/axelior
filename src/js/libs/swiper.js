a72472650e90f5a3df964a79228692f0debb3ca8
Igor Bodrov <bodrovigor02@gmail.com>
2023-06-26 14:02:08 +0300

    fix: 🐛 bugfix

--- src/js/libs/swiper.js
+++ src/js/libs/swiper.js
@@ -39,41 +39,27 @@ let index_catalogue_slider = new Swiper(".index-catalogue__slider", {
   },
 });
 
-var partners_slider_init = false;
+let partners_slider = new Swiper(".partners__slider", {
+  direction: "horizontal",
+  spaceBetween: 40,
 
-function partners_slider() {
-  if (window.innerWidth <= 1279) {
-    if (!partners_slider_init) {
-      partners_slider_init = true;
-      var partners_slider = new Swiper(".partners__slider", {
-        direction: "horizontal",
-        spaceBetween: 40,
-
-        breakpoints: {
-          320: {
-            slidesPerView: 1
-          },
-          768: {
-            slidesPerView: 2,
-          },
-          1024: {
-            slidesPerView: "auto",
-          }
-        },
-
-        pagination: {
-          el: ".partners__slider-pagination",
-          type: "progressbar",
-        },
-      });
+  breakpoints: {
+    320: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: "auto",
     }
-  } else if (partners_slider_init) {
-    partners_slider.destroy();
-    partners_slider_init = false;
-  }
-}
-partners_slider();
-window.addEventListener("resize", partners_slider);
+  },
+
+  pagination: {
+    el: ".partners__slider-pagination",
+    type: "progressbar",
+  },
+});
 
 var letters_slider_init = false;
 
