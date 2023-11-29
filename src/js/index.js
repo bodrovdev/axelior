16d810717ec3fb9cd244e9ab300b5c18fed89784
Igor Bodrov <bodrovigor02@gmail.com>
2023-11-29 09:53:18 +0300

    fix: 🐛 bugfix

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import { lock, unlock } from 'tua-body-scroll-lock';
 
-// --- Мобильное меню
+// ? --- Мобильное меню
 const burger = document.querySelector('#burger');
 const side_menu = document.querySelector('.side-menu');
 const side_menu_links = document.querySelectorAll('.side-menu__main-nav-link');
@@ -68,22 +68,22 @@ function toggleByClick(element) {
     })
 };
 
-// --- Смена активного класса в блоке ссылок на всех страницах
+// ? --- Смена активного класса в блоке ссылок на всех страницах
 changeActive('links__list-item');
 
-// --- Смена активного класса в селектах в боковом меню
+// ? --- Смена активного класса в селектах в боковом меню
 changeActive('side-menu__main-nav-select');
 
-// --- Сброс активного класса при клике на пустое место в селектах в боковом меню
+// ? --- Сброс активного класса при клике на пустое место в селектах в боковом меню
 toggleByClick('side-menu__main-nav-select');
 
-// --- Табы на странице категорий
+// ? --- Табы на странице категорий
 window.addEventListener('load', () => {
     if (document.querySelector('#page-categories') === null) {
         return;
     }
     else {
-        // - Переход к табам при загрузке на странице категорий, или при изменении хэша страница
+        // ? - Переход к табам при загрузке на странице категорий, или при изменении хэша страница
         let tabs_buttons = document.querySelectorAll('.links__list-item');
         let tabs_content = document.querySelectorAll('.page-categories__tabs-content');
         let tabs_links = Array.from(document.querySelectorAll(`.side-menu ul a`)).filter((link) => link.href.split('').includes('#'));
@@ -121,7 +121,7 @@ window.addEventListener('load', () => {
         tabs();
         window.addEventListener('hashchange', () => { tabs() });
 
-        // - Смена табов по клику
+        // ? - Смена табов по клику
         tabs_buttons.forEach((button_item) => {
             button_item.addEventListener('click', () => {
                 tabs_buttons.forEach((button_value) => { button_value.classList.remove((`${button_value.classList[0]}--active`)) });
@@ -148,7 +148,7 @@ window.addEventListener('load', () => {
 })
 
 
-// --- Модалка с формой
+// ? --- Модалка с формой
 window.addEventListener('load', () => {
     if (document.querySelector('#modal_with_form') === null) {
         return;
@@ -164,7 +164,7 @@ window.addEventListener('load', () => {
             unlock(modal_with_form);
         }
 
-        // - Открытие модалки на нажатие кнопки
+        // ? - Открытие модалки на нажатие кнопки
         modal_with_form_buttons.forEach((button) => {
             button.addEventListener('click', () => {
                 modal_with_form.classList.add('modal-with-form--active');
@@ -172,14 +172,14 @@ window.addEventListener('load', () => {
             })
         })
 
-        // - Закрытие модалки по нажатию крестика
+        // ? - Закрытие модалки по нажатию крестика
         modal_with_form_close.addEventListener('click', () => {
             closeFormModal();
             document.querySelector('.modal-with-form__inner').classList.remove('modal-with-form__inner--hidden');
             document.querySelector('.modal-with-form__success').classList.remove('modal-with-form__success--active');
         })
 
-        // - Закрытие модалки по нажатию на пустое место
+        // ? - Закрытие модалки по нажатию на пустое место
         modal_with_form.addEventListener('click', (e) => {
             if (e.target !== e.currentTarget) {
                 return;
@@ -191,7 +191,7 @@ window.addEventListener('load', () => {
             }
         })
 
-        // - Подтверждение отправки модалки
+        // ? - Подтверждение отправки модалки
         modal_with_form_formset.addEventListener('submit', (e) => {
             e.preventDefault();
             document.querySelector('.modal-with-form__inner').classList.add('modal-with-form__inner--hidden');
@@ -200,7 +200,7 @@ window.addEventListener('load', () => {
     }
 })
 
-// --- Модалка без формы
+// ? --- Модалка без формы
 window.addEventListener('load', () => {
     if (document.getElementById('modal_without_form') === null) {
         return;
@@ -215,7 +215,7 @@ window.addEventListener('load', () => {
             unlock(modal_without_form);
         }
 
-        // - Появление модалки без формы на событие отправка формы
+        // ? - Появление модалки без формы на событие отправка формы
         modal_without_form_formset.forEach((form) => {
             form.addEventListener('submit', (e) => {
                 e.preventDefault();
@@ -224,12 +224,12 @@ window.addEventListener('load', () => {
             })
         });
 
-        // - Закрытие модалки без формы по нажатию крестика
+        // ? - Закрытие модалки без формы по нажатию крестика
         modal_without_form_close.addEventListener('click', () => {
             closeWithoutFormModal();
         })
 
-        // - Закрытие модалки без формы по нажатию на пустое место
+        // ? - Закрытие модалки без формы по нажатию на пустое место
         modal_without_form.addEventListener('click', (e) => {
             if (e.target !== e.currentTarget) {
                 return;
@@ -241,7 +241,7 @@ window.addEventListener('load', () => {
     }
 })
 
-// --- Отключение подсветки ошибок в инпутах
+// ? --- Отключение подсветки ошибок в инпутах
 window.addEventListener('load', () => {
     if (document.querySelectorAll('input') === null) {
         return;
